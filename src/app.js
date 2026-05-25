e6b5c2d5e13eba135a1deb97e12198e27adccc65
mariamtarek51 <mariamtarek51@gmail.com>
2026-05-25 17:53:20 +0300

    add package.json and test.sh

---
+++ src/app.js
@@ -0,0 +1,10 @@
+function greet(name) {
+
+return 'Hello, ${name}!';
+}
+module. exports = greet;
+// Ensure function is exportable
+// If run directly, print Output (useful for debugging)
+if (require.main ===module) {
+console. tog (greet( "World" ) ) ;
+}
