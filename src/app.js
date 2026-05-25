d7da6bf2becc7722693bc9f7a6798c98ad71586d
mariamtarek51 <mariamtarek51@gmail.com>
2026-05-25 18:04:01 +0300

    unnesseccary code

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ return `Hello, ${name}!`;
 module. exports = greet;
 // Ensure function is exportable
 // If run directly, print Output (useful for debugging)
-if (require.main ===module) {
-console. tog (greet( "World" ) ) ;
-}
+//if (require.main ===module) {
+//console. tog (greet( "World" ) ) ;
+//}
